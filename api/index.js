d78b8191313407c5d43864d90b77f6ea7b56ed95
Miguel2493 <a2493m@gmail.com>
2021-08-22 00:14:36 -0500

    CreatedUsers Completed

--- api/index.js
+++ api/index.js
@@ -1,12 +1,62 @@
+require(`dotenv`).config();
 const express = require('express');
 const apiRouter = express.Router();
 const postsRouter = require('./posts');
 const usersRouter = require('./users');
 const tagsRouter = require('./tags');
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
+console.log(process.env.JWT_SECRET);
+
+// set `req.user` if possible
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) { // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    });
+  }
+});
+
+// Attach routers below here
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/posts', postsRouter);
 apiRouter.use('/tags', tagsRouter);
+apiRouter.get('/', (req,res,next)=>{
+    res.send('In the API!')
+})
+apiRouter.use((error,req,res,next)=>{
+    res.send(error);
+});
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
 
 
 module.exports = apiRouter;
